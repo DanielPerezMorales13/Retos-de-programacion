49c59d985a96e7cfea2a381c10ad4789e7058bdd
Daniel_Perez <138601001+Danitrix13@users.noreply.github.com>
2023-12-26 17:29:05 -0600

    ejercicio 1 lenguages = [     python,     C,     Java ]

---
+++ Algoritmos_de_programacion_2024/C/algoritmo#1/src/ejercicio.c
@@ -0,0 +1,64 @@
+/* # #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+> #### Dificultad: Fácil | Publicación: 26/12/23 | Corrección: 02/01/24
+
+## Ejercicio
+
+```javascript
+
+  ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+  - Recuerda que todas las instrucciones de participación están en el
+    repositorio de GitHub.
+
+  Lo primero... ¿Ya has elegido un lenguaje?
+  - No todos son iguales, pero sus fundamentos suelen ser comunes.
+  - Este primer reto te servirá para familiarizarte con la forma de participar
+    enviando tus propias soluciones.
+
+  EJERCICIO:
+  - Crea un comentario en el código y coloca la URL del sitio web oficial del
+    lenguaje de programación que has seleccionado.
+  - Representa las diferentes sintaxis que existen de crear comentarios
+    en el lenguaje (en una línea, varias...).
+  - Crea una variable (y una constante si el lenguaje lo soporta).
+  - Crea variables representando todos los tipos de datos primitivos
+    del lenguaje (cadenas de texto, enteros, booleanos...).
+  - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+  ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+  debemos comenzar por el principio.
+ /
+```
+#### Tienes toda la información extendida sobre el roadmap de retos de programación en [retosdeprogramacion.com/roadmap](https://retosdeprogramacion.com/roadmap).
+
+Sigue las [instrucciones](../../README.md), consulta las correcciones y aporta la tuya propia utilizando el lenguaje de programación que quieras.
+
+> Recuerda que cada semana se publica un nuevo ejercicio y se corrige el de la semana anterior en directo desde [Twitch](https://twitch.tv/mouredev). Tienes el horario en la sección "eventos" del servidor de [Discord](https://discord.gg/mouredev).
+
+*/
+
+#include <stdio.h>
+
+void main(void)
+{
+  // comentario de una linea:  
+
+  /*
+  comentario
+  de
+  varias
+  lineas
+  */
+
+  // Tipos de datos
+  short int integer = 1;
+  long int integer_two = 1;
+  char character = 'a';
+  char character_saludo[] = "Hola lenguage C";
+  float float_numero = 2.5;
+  double double_numero = 2.5;
+  size_t size_t_numero = 2;
+
+  // Imprimir por terminal
+  puts(character_saludo);
+  return;
+}
---
+++ Algoritmos_de_programacion_2024/Python/algoritmo#1/src/ejercicio.py
@@ -0,0 +1,46 @@
+"""/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+"""
+
+# comentario de una linea ==> URL: https://www.python.org/
+
+"""
+comentario 
+de 
+multiples
+lineas
+"""
+
+# tipos de datos
+
+def main() -> str:
+    string: str = "Daniel Perez"
+    integer: int = 18
+    boolean: bool = True 
+    boolean: bool = False
+    return "Hola Python"
+
+if __name__ == "__main__":
+    print(main())
+
